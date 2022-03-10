dffe34634bc3dea7cf1d289621b12af8646709e1
Donghee Kim <dongheekim@woowahan.com>
2022-03-10 14:26:37 +0900

    feat: 파일명에 draft: 가 포함된 md는 PostData에서 제외한다

--- lib/posts.ts
+++ lib/posts.ts
@@ -9,22 +9,29 @@ type MatterData = { title: string; date: string; summary: string };
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
+const sortByDateDesc = ({ date: a }: MatterData, { date: b }: MatterData) =>
+  b.localeCompare(a);
+
+const isDraft = (fileName: string) => /^draft:/i.test(fileName);
+
 export const getSortedPostsData = () => {
   const fileNames = readdirSync(postsDirectory);
 
-  const allPostsData = fileNames.map((fileName) => {
-    const id = fileName.replace(/\.md$/, "");
+  const allPostsData = fileNames
+    .filter((fileName) => !isDraft(fileName))
+    .map((fileName) => {
+      const id = fileName.replace(/\.md$/, "");
 
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = readFileSync(fullPath, "utf8");
+      const fullPath = path.join(postsDirectory, fileName);
+      const fileContents = readFileSync(fullPath, "utf8");
 
-    const matterResult = matter(fileContents);
-    const data = matterResult.data as MatterData;
+      const matterResult = matter(fileContents);
+      const data = matterResult.data as MatterData;
 
-    return { id, ...data };
-  });
+      return { id, ...data };
+    });
 
-  return allPostsData.sort(({ date: a }, { date: b }) => b.localeCompare(a));
+  return allPostsData.sort(sortByDateDesc);
 };
 
 export const getAllPostIds = () =>
