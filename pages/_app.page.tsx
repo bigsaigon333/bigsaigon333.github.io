a7cd56d9b88eba2c4c3c309dbae1373d8532952c
Donghee Kim <dongheekim@woowahan.com>
2022-03-10 20:42:56 +0900

    fix: viewport meta tag를 _app.page.tsx 로 이동시킨다
    https://nextjs.org/docs/messages/no-document-viewport-meta

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,10 +1,14 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 import GlobalStyle from "../styles/global-style";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -38,12 +38,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="ko">
-        <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
