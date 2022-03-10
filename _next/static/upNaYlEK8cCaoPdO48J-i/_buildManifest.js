1d04355a125e05db80b7d21be6782d228d21deec
Donghee Kim <dongheekim@woowahan.com>
2022-03-10 14:36:58 +0900

    Updates

---
+++ _next/static/upNaYlEK8cCaoPdO48J-i/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8d77ee50b516f0a1.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/posts/[id]":["static/chunks/pages/posts/[id]-1c997ea649806470.js"],sortedPages:["/","/_app","/_error","/posts/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
