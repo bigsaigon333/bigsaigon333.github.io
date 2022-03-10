6b1574f0b3c3421ffe7c1f4a1c855182825beff3
Donghee Kim <dongheekim@woowahan.com>
2022-03-10 21:44:00 +0900

    Updates

---
+++ _next/static/55uO-wIUsCNXzo3FLzsBt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-fbf951fcf00ef139.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/posts/[id]":["static/chunks/pages/posts/[id]-d175f964442ca6f8.js"],sortedPages:["/","/_app","/_error","/posts/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
