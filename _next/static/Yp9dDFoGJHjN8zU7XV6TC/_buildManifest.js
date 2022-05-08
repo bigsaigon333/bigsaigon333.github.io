cdc99197af3c181e2cb319422d1e033087b8fe98
Donghee Kim <dongheekim@woowahan.com>
2022-05-08 23:52:47 +0900

    Updates

--- _next/static/Yp9dDFoGJHjN8zU7XV6TC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-398df86a5f76b40e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/posts/[id]":["static/chunks/pages/posts/[id]-bd0fea9977d95d65.js"],sortedPages:["/","/_app","/_error","/posts/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
