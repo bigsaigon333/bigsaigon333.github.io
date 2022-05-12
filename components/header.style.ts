bac6744c6e1b1de65c1178d7b56dabd98bbfbf97
Donghee Kim <dongheekim@woowahan.com>
2022-05-12 20:49:14 +0900

    feat: 헤더 타이틀에 홈으로 가는 링크를 추가한다

--- components/header.style.ts
+++ components/header.style.ts
@@ -11,18 +11,22 @@ export const Title = styled.h1`
   margin: 0;
   line-height: 1.25;
   width: 100%;
-  font-size: 3.6rem;
-  text-align: center;
-  display: flex;
-  column-gap: 2rem;
-  justify-content: center;
-  align-items: center;
   font-weight: 800;
 
   @media (max-width: 767px) {
     font-size: 7.5vw;
     column-gap: 1rem;
   }
+`;
+
+export const A = styled.a`
+  width: 100%;
+  font-size: 3.6rem;
+  text-align: center;
+  display: flex;
+  column-gap: 2rem;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const ProfileWrapper = styled.div`
