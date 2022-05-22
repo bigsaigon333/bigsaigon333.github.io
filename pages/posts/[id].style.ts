91fa8d14587e7a5cbfb50f31de1c9320a720123f
Donghee Kim <dongheekim@woowahan.com>
2022-05-22 23:03:08 +0900

    feat: 포스트의 스타일을 추가한다

--- pages/posts/[id].style.ts
+++ pages/posts/[id].style.ts
@@ -3,4 +3,17 @@ import styled from "styled-components";
 export const Main = styled.main`
   flex-grow: 1;
   line-height: 1.75;
+
+  a {
+    word-break: break-word;
+  }
+
+  a:hover {
+    box-shadow: 0 0.125rem 0 0 black;
+  }
+
+  ul {
+    margin: 0;
+    padding-inline-start: 20px;
+  }
 `;
