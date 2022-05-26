49dcf422f2e94a968ef1f420a56214d41a66bbbc
Donghee Kim <dongheekim@woowahan.com>
2022-05-26 21:16:58 +0900

    feat: `` 마크다운이 변환되는 code 요소에 스타일을 추가한다

--- pages/posts/[id].style.ts
+++ pages/posts/[id].style.ts
@@ -16,4 +16,12 @@ export const Main = styled.main`
     margin: 0;
     padding-inline-start: 20px;
   }
+
+  & :not(pre) > code {
+    border-radius: 0.3em;
+    background: rgb(255 229 100 / 20%);
+    color: #1a1a1a;
+    padding: 0.15em 0.2em 0.05em;
+    white-space: normal;
+  }
 `;
