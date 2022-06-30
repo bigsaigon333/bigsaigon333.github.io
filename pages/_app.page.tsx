430b0752896b0d9c3aadf237a4e993ff115b2337
Donghee Kim <dongheekim@woowahan.com>
2022-06-30 18:37:31 +0900

    feat: GA를 부착한다

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 
 import GlobalStyle from "../styles/global-style";
 
@@ -9,6 +10,19 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-Y270VJN1Y2"
+      ></Script>
+      <Script id="ga">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Y270VJN1Y2');
+        `}
+      </Script>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
