856946399f17cf0e29edfa1247bbda21a2d78d05
bigsaigon333 <likepeppermint@gmail.com>
2023-05-14 18:55:45 +0900

    feat: 메인페이지에 OG meta 태그 추가

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -29,6 +29,28 @@ const Home = ({
           content="김동희,프론트엔드,개발,자바스크립트,타입스크립트,JS,javascript,typescript,TS,react"
         />
         <link rel="canonical" href="https://bigsaigon333.github.io" />
+
+        <meta property="og:title" content="김동희의 개발블로그" />
+        <meta
+          property="og:description"
+          content="프론트엔드 개발과 관련된 이야기를 다룹니다."
+        />
+        <meta property="og:url" content="https://bigsaigon333.github.io" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://bigsaigon333.github.io/main-og-image.jpg"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="김동희의 개발블로그" />
+        <meta
+          name="twitter:description"
+          content="프론트엔드 개발과 관련된 이야기를 다룹니다."
+        />
+        <meta
+          name="twitter:image"
+          content="https://bigsaigon333.github.io/main-og-image.jpg"
+        />
       </Head>
 
       <Section>
