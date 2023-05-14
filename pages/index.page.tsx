0d3ad3b43c6c95e036b97e539d8d4e80b3c75bce
bigsaigon333 <likepeppermint@gmail.com>
2023-05-14 18:39:34 +0900

    feat: 메인페이지에 SEO meta 태그 추가

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -18,14 +18,23 @@ const Home = ({
   return (
     <Layout home>
       <Head>
-        <title>김동희입니다</title>
-        <meta name="description" content="김동희의 블로그" />
+        <title>김동희의 개발블로그</title>
+        <meta
+          name="description"
+          content="프론트엔드 개발과 관련된 이야기를 다룹니다."
+        />
+        <meta name="author" content="김동희" />
+        <meta
+          name="keywords"
+          content="김동희,프론트엔드,개발,자바스크립트,타입스크립트,JS,javascript,typescript,TS,react"
+        />
+        <link rel="canonical" href="https://bigsaigon333.github.io" />
       </Head>
 
       <Section>
         {allPostsData.map(({ id, title, date }) => (
           <Article key={id}>
-            <Link href={`/posts/${id}`} passHref>
+            <Link href={`/posts/${id}`}>
               <h2>{title}</h2>
             </Link>
             <time>{date}</time>
